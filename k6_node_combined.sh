4b311e0ea013ab74285ffaf6aab8b66015653957
Daniel Patrick C. Marquez <danielpatrick.marquez@isentia.com>
2023-04-14 16:57:47 +0800

    Update shell script and README to accept parameters in command line

--- k6_node_combined.sh
+++ k6_node_combined.sh
@@ -1,5 +1,5 @@
 echo "*** running k6 ***"
 logfile="$(date +%Y-%m-%dT%H:%M:%S)_loadtest.log"
-k6 run --console-output ${logfile} -e AGG_SIZE=10 -e QUERY_LENGTH=3 -e QUERY_TYPE=Simple k6-script.js
+k6 run --console-output ${logfile} -e AGG_SIZE=$1 -e QUERY_LENGTH=$2 -e QUERY_TYPE=$3 k6-script.js
 echo "*** running node ***"
 node readlog.js ${logfile}

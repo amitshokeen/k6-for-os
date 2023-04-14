b8adc280ba633d7593b72dc2c7b7aba58652b31a
Amit Shokeen <ashokeen@Amits-MacBook-Pro.local>
2023-04-14 10:10:18 +1000

    min, max, avg of all 'took' values

--- readlog.js
+++ readlog.js
@@ -2,15 +2,17 @@ const fs = require('fs');
 const fileName = process.argv.slice(2);
 try {
   const data = fs.readFileSync(`./${fileName}`, 'utf8');
-  console.log(data);
+  //console.log(data);
   let dataArr = data.split(" ");
   const reg = new RegExp('^[0-9]+$');
-  let myFinalArr = [];
-  dataArr.forEach(e => reg.test(e) && myFinalArr.push(e))
-
+  const myFinalArr = dataArr.filter(e => reg.test(e)).map(x => parseInt(x))
   // TODO: write this myFinalArr to a separate file
   // and find a way to plot a chart, extract tha vlues for avg, min, med, max, p(90) and p(95)
   console.log(myFinalArr); 
+  console.log("Minimum value: " + Math.min(...myFinalArr));
+  console.log("Maximum value: " + Math.max(...myFinalArr));
+  console.log("Average value: " + myFinalArr.reduce((a, b) => a + b, 0) / myFinalArr.length);
+  
 } catch (err) {
   console.error(err);
 }

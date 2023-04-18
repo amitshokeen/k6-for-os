0ac359f9cb37ebd66584f634e4e5468dfcd9d111
Amit Shokeen <ashokeen@Amits-MacBook-Pro.local>
2023-04-18 17:31:48 +1000

    shell file for XTEAM-2293

---
+++ k6_XTEAM-2293.sh
@@ -0,0 +1,3 @@
+echo "*** running k6 ***"
+logfile="$(date +%Y-%m-%dT%H:%M:%S)_loadtest_XTEAM-2293.log"
+k6 run --console-output ${logfile} -e AGG_SIZE=$1 -e QUERY_LENGTH=$2 -e QUERY_TYPE=$3 k6-script_XTEAM-2293.js

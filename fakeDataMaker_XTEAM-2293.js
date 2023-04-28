697edc7606ca91fe1b3c4fececbb9733652fccc2
Daniel Patrick C. Marquez <danielpatrick.marquez@isentia.com>
2023-04-28 09:06:05 +0800

    Update wildcard query

--- fakeDataMaker_XTEAM-2293.js
+++ fakeDataMaker_XTEAM-2293.js
@@ -50,15 +50,6 @@ export function fakeQueryString() {
         }
     }
     else {
-        // if (query_type == "Wildcard") {
-        //     wildcard = Array(faker.lorem.word({ length: { min: 1, max: 3 } }), faker.random.word({ length: { min: 3 } }));
-        //     random_word = wildcard[Math.floor(Math.random() * wildcard.length)];
-        //     while (random_word.indexOf(' ') >= 0) {
-        //         wildcard = Array(faker.lorem.word({ length: { min: 1, max: 3 } }), faker.random.word({ length: { min: 3 } }));
-        //         random_word = wildcard[Math.floor(Math.random() * wildcard.length)];
-        //     }
-        //     final_query_string = random_word + "*"; // * in the middle of the word, * after a word, * after a two letter word
-        // }
         if (query_type = "Wildcard") {
             wildcard = faker.lorem.word({ length: { min: 1, max: 3 } });
             while (wildcard.length > 3) {
@@ -70,17 +61,25 @@ export function fakeQueryString() {
             }
             random_word = Array(wildcard, wildword);
             random_word = random_word[Math.floor(Math.random() * random_word.length)];
-            letter_replace = Math.floor((Math.random() * random_word.length));
-            random_word = random_word.split("");
-            console.log(letter_replace)
-            if (random_word.length > 1) {
-                for (var i = 0; i < random_word.length; i++) {
-                    if (i == letter_replace) {
-                        random_word[i] = "*";
-                    }
-                    final_query_string += random_word[i];
-                }
+
+            if (random.word.length > 1) {
+                final_query_string = random_word.replace([...random_word][Math.floor(Math.random() * [...random_word].length)], "*");
             }
+            else {
+                final_query_string = random_word + "*";
+            }
+
+            // letter_replace = Math.floor((Math.random() * random_word.length));
+            // random_word = random_word.split("");
+            // console.log(letter_replace)
+            // if (random_word.length > 1) {
+            //     for (var i = 0; i < random_word.length; i++) {
+            //         if (i == letter_replace) {
+            //             random_word[i] = "*";
+            //         }
+            //         final_query_string += random_word[i];
+            //     }
+            // }
         }
         else {
             random_word = faker.random.word({ length: { min: 3 } })
